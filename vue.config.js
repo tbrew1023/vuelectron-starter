c88ec6723ab4d6a7c4b4110035fbc2aa5f762d38
Trent Brew <tbrew1023@gmail.com>
2020-11-18 03:47:13 -0600

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    configureWebpack: config => {
+      if (process.env.NODE_ENV === 'production') {
+        config.output.publicPath = `${process.cwd()}/dist/`
+      }
+    }
+  }
